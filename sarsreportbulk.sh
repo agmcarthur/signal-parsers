21e43d0864436179e4c8e54261605960996ff170
Andrew McArthur <mcarthua@mcmaster.ca>
2020-07-07 09:27:27 -0400

    csh to sh

--- sarsreportbulk.sh
+++ sarsreportbulk.sh
@@ -1,18 +1,12 @@
-#!/bin/csh
+#!/bin/sh
 
 echo "assembly_report.pl"
 rm -rf result*
 rm -rf bioproject_fastq
-foreach file (`ls`)
-	cd $file
-	assembly_report.pl
-	cd ..
-end
+for file in `ls | grep -v summary | grep -v config`; do cd $file; assembly_report.pl; cd ..; done
 echo "assembly_summarize.pl"
 assembly_summarize.pl
 echo "assembly_mutations.pl"
 assembly_mutations.pl
 echo "prepbioproject.pl"
 prepbioproject.pl
-
-
