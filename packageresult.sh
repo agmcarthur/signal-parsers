fb9bb78b2007302a8755a81b7befc394ff89da2c
Andrew McArthur <mcarthua@mcmaster.ca>
2020-07-07 11:21:40 -0400

    separate host free fastq from results

--- packageresult.sh
+++ packageresult.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-tar cvf /home/mcarthua/$1.tar bioproject_fastq results results_mutations_freq.tsv results_mutations.tsv results.tsv summary*
+tar cvf /home/mcarthua/$1_fastq.tar bioproject_fastq
+tar cvf /home/mcarthua/$1_results.tar results results_mutations_freq.tsv results_mutations.tsv results.tsv summary*
 gzip /home/mcarthua/$1.tar
-chgrp mcarthua /home/mcarthua/$1.tar.gz
-chown mcarthua /home/mcarthua/$1.tar.gz
+chgrp mcarthua /home/mcarthua/$1*
+chown mcarthua /home/mcarthua/$1*
