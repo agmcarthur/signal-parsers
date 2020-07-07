163e66cf642b1472dffc204f526615c1330cbaa0
Andrew McArthur <mcarthua@mcmaster.ca>
2020-07-07 10:41:02 -0400

    avoid unwanted directories

--- assembly_summarize.pl
+++ assembly_summarize.pl
@@ -4,16 +4,18 @@ chomp(@directories=<*>);
 system("mkdir results");
 
 foreach $directory (@directories) {
-	system("cp $directory/*.png results/.");
-	system("cp $directory/*report* results/.");
-	system("cp $directory/*mutations* results/.");
-	system("cp $directory/*statistic* results/.");
-	system("cp $directory/*.fa results/.");
-	open (INPUT,"< $directory/${directory}_statistics.tsv");
-	chomp($header=<INPUT>);
-	chomp($data=<INPUT>);
-	$results{$directory}=$data;
-	close (INPUT);
+	unless ($directory =~ /summary/ or $directory =~ /config/) {
+		system("cp $directory/*.png results/.");
+		system("cp $directory/*report* results/.");
+		system("cp $directory/*mutations* results/.");
+		system("cp $directory/*statistic* results/.");
+		system("cp $directory/*.fa results/.");
+		open (INPUT,"< $directory/${directory}_statistics.tsv");
+		chomp($header=<INPUT>);
+		chomp($data=<INPUT>);
+		$results{$directory}=$data;
+		close (INPUT);
+	}
 }
 open (OUTPUT,"> results.tsv");
 print OUTPUT "Sample\t$header\n";
--- prepbioproject.pl
+++ prepbioproject.pl
@@ -4,7 +4,7 @@ chomp(@directories=<*>);
 system("mkdir bioproject_fastq");
 
 foreach $directory (@directories) {
-	unless ($directory =~ /results/ or $directory =~ /bioproject_fastq/) {
+	unless ($directory =~ /results/ or $directory =~ /bioproject_fastq/ or $directory =~ /config/ or $directory =~ /summary/) {
 		system("cp $directory/host_removal/${directory}_R1.fastq.gz bioproject_fastq/${directory}_R1.fastq.gz");
 		system("cp $directory/host_removal/${directory}_R2.fastq.gz bioproject_fastq/${directory}_R2.fastq.gz");
 	}
