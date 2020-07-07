13d8d20e93acfbed1bc11b998c91e4ec35ac1580
Andrew McArthur <mcarthua@mcmaster.ca>
2020-07-07 16:12:52 -0400

    fiz gzip

--- packageresult.sh
+++ packageresult.sh
@@ -2,6 +2,7 @@
 
 tar cvf /home/mcarthua/$1_fastq.tar bioproject_fastq
 tar cvf /home/mcarthua/$1_results.tar results results_mutations_freq.tsv results_mutations.tsv results.tsv summary*
-gzip /home/mcarthua/$1.tar
+gzip /home/mcarthua/$1_fastq.tar
+gzip /home/mcarthua/$1_results.tar
 chgrp mcarthua /home/mcarthua/$1*
 chown mcarthua /home/mcarthua/$1*
