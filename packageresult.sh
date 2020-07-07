dd0c5d385a12896d3517c1f7be7b2481cf07d4c2
Andrew McArthur <mcarthua@mcmaster.ca>
2020-07-07 11:05:14 -0400

    fix ownerships

--- packageresult.sh
+++ packageresult.sh
@@ -2,3 +2,5 @@
 
 tar cvf /home/mcarthua/$1.tar bioproject_fastq results results_mutations_freq.tsv results_mutations.tsv results.tsv summary*
 gzip /home/mcarthua/$1.tar
+chgrp mcarthua /home/mcarthua/$1.tar.gz
+chown mcarthua /home/mcarthua/$1.tar.gz
