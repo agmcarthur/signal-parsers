46af3d02b9a6ef096f61c95ada6cec7136194a13
Andrew McArthur <mcarthua@mcmaster.ca>
2020-10-01 17:44:47 -0400

    package NCoV update

--- packagencov.sh
+++ packagencov.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-tar cvf /home/mcarthua/$1_ncov.tar plots qc_analysis
+tar cvf /home/mcarthua/$1_ncov.tar *
 gzip /home/mcarthua/$1_ncov.tar
 chgrp mcarthua /home/mcarthua/$1*
 chown mcarthua /home/mcarthua/$1*
