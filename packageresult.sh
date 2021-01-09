aa4efefb1653285935fb059698c064acfdd6313f
Andrew McArthur <mcarthua@mcmaster.ca>
2021-01-09 10:02:10 -0500

    capture variants files too

--- packageresult.sh
+++ packageresult.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 tar cvf /home/mcarthua/$1_fastq.tar bioproject_fastq
-tar cvf /home/mcarthua/$1_results.tar results results_mutations_freq.tsv results_mutations.tsv results.tsv summary*
+tar cvf /home/mcarthua/$1_results.tar results variants results_mutations_freq.tsv results_mutations.tsv results.tsv summary*
 gzip /home/mcarthua/$1_fastq.tar
 gzip /home/mcarthua/$1_results.tar
 chgrp mcarthua /home/mcarthua/$1*
