582c32e359d274184b806c059f4aaccfc893bd05
Andrew McArthur <mcarthua@mcmaster.ca>
2021-03-02 11:13:49 -0500

    new directory structure (pangolin)

--- packageresult.sh
+++ packageresult.sh
@@ -3,7 +3,7 @@
 rm -rf /home/mcarthua/$1_*
 mkdir /home/mcarthua/$1_fastq
 cp bioproject_fastq/* /home/mcarthua/$1_fastq/.
-tar cvf /home/mcarthua/$1_results.tar results variants results_mutations_freq.tsv results_mutations.tsv results.tsv pangolin summary*
+tar cvf /home/mcarthua/$1_results.tar results results_mutations_freq.tsv results_mutations.tsv results.tsv pangolin summary*
 gzip /home/mcarthua/$1_results.tar
 chgrp mcarthua /home/mcarthua/$1_*
 chown mcarthua /home/mcarthua/$1_*
