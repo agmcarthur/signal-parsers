0387f5ab90978ddfcfcc45976451dabc03c083dd
Andrew McArthur <mcarthua@mcmaster.ca>
2021-03-02 11:02:58 -0500

    update package scripts

--- packageresult.sh
+++ packageresult.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-tar cvf /home/mcarthua/$1_fastq.tar bioproject_fastq
-tar cvf /home/mcarthua/$1_results.tar results variants results_mutations_freq.tsv results_mutations.tsv results.tsv summary*
-gzip /home/mcarthua/$1_fastq.tar
+rm -rf /home/mcarthua/$1_*
+mkdir /home/mcarthua/$1_fastq
+cp bioproject_fastq/* /home/mcarthua/$1_fastq/.
+tar cvf /home/mcarthua/$1_results.tar results variants results_mutations_freq.tsv results_mutations.tsv results.tsv pangolin summary*
 gzip /home/mcarthua/$1_results.tar
-chgrp mcarthua /home/mcarthua/$1*
-chown mcarthua /home/mcarthua/$1*
+chgrp mcarthua /home/mcarthua/$1_*
+chown mcarthua /home/mcarthua/$1_*
